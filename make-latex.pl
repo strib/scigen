6933683a93f2091b860c01add1302167e64f8f01
David Pomerenke <david.pomerenke@uni-konstanz.de>
2020-01-12 17:45:29 +0000

    fix bug
    Fixed bug "Can't locate scigen.pm in @INC [...] at ./make-latex.pl line 21." & similar bugs by including current directory as library.

--- make-latex.pl
+++ make-latex.pl
@@ -16,7 +16,7 @@
 #    along with SCIgen; if not, write to the Free Software
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-
+use lib '.';
 use strict;
 use scigen;
 use IO::File;
