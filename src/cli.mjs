7418f29f1a543948c4462d454a1fc6cfb1351fe6
David Pomerenke <david.pomerenke@uni-konstanz.de>
2021-11-30 16:03:31 +0100

    Improve API

--- src/cli.mjs
+++ src/cli.mjs
@@ -4,18 +4,20 @@ import fs from "fs";
 import path from "path";
 import { scigen } from "./scigen";
 
-export const saveScigen = (
-  authors,
-  bibinlatex,
-  directory = "scigen",
-  silent
+export const scigenSave = (
+  { directory, authors, useBibtex, silent } = {
+    directory: "scigen",
+    authors: undefined,
+    useBibtex: false,
+    silent: false,
+  }
 ) => {
   directory = path.resolve(...directory.split(/\\|\//g));
   if (!fs.existsSync(directory)) {
     fs.mkdirSync(directory);
   }
   const files = {
-    ...scigen(authors, bibinlatex).files,
+    ...scigen({ authors: authors, useBibtex: useBibtex }),
     "IEEEtran.cls": fs.readFileSync(path.resolve(__dirname, "IEEEtran.cls")),
     "IEEE.bst": fs.readFileSync(path.resolve(__dirname, "IEEE.bst")),
   };
@@ -25,38 +27,36 @@ export const saveScigen = (
   if (!silent) {
     console.log(
       `Saved in ${path.resolve(directory)}.\n` +
-        "Run\n" +
+        "Run\n\n" +
         `\tcd ${path.resolve(directory)}\n` +
         "\tpdflatex paper.tex\n" +
-        ("bibinlatex" in argv && argv.bibinlatex
-          ? ""
-          : "\tbibtex paper.aux\n") +
-        ("bibinlatex" in argv && argv.bibinlatex
-          ? ""
-          : "\tpdflatex paper.tex\n") +
-        ("bibinlatex" in argv && argv.bibinlatex
-          ? ""
-          : "\tpdflatex paper.tex\n") +
-        "to compile to PDF."
+        (useBibtex
+          ? "\tbibtex paper.aux\n\tpdflatex paper.tex\n\tpdflatex paper.tex\n"
+          : "") +
+        +"\nto compile to PDF."
     );
   }
 };
 
-const argv = minimist(process.argv.slice(2));
-if ("save" in argv && argv.save) {
-  saveScigen(
-    (argv.authors ? argv.authors.split(",").map((l) => l.trim()) : undefined) ||
-      (argv.author ? argv.author.split(",").map((l) => l.trim()) : undefined),
-    argv.bibinlatex,
-    argv.save || argv.dir,
-    argv.quiet || argv.silent
-  );
-} else {
-  console.log(
-    'Usage: node cli.js --save [<directory>] [--authors "<author1>, <author2>, ..."] [--bibinlatex] [--silent]\n' +
-      "\tdirectory \tall files (.tex, .eps, .cls, .bib, ...) will be saved here\n" +
-      "\tauthors \tlist of the authors in the paper\n" +
-      "\tbibinlatex \tavoids dependency on BibTex (useful especially for texlive.js)\n" +
-      "\tsilent \t\tskip info logging"
-  );
+if (require.main === module) {
+  const argv = minimist(process.argv.slice(2));
+  if ("save" in argv && argv.save) {
+    scigenSave(
+      (argv.authors
+        ? argv.authors.split(",").map((l) => l.trim())
+        : undefined) ||
+        (argv.author ? argv.author.split(",").map((l) => l.trim()) : undefined),
+      argv.useBibtex,
+      argv.save || argv.dir,
+      argv.quiet || argv.silent
+    );
+  } else {
+    console.log(
+      'Usage: node cli.js --save [<directory>] [--authors "<author1>, <author2>, ..."] [--useBibtex] [--silent]\n' +
+        "\tdirectory \tall files (.tex, .eps, .cls, .bib, ...) will be saved here\n" +
+        "\tauthors \tlist of the authors in the paper\n" +
+        "\tuseBibtex \tuse Bibtex for formatting the bibliography (disable this for use with texlive.js)\n" +
+        "\tsilent \t\tskip info logging"
+    );
+  }
 }
